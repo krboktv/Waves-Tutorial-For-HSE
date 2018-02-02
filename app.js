5df58a184f9cee05b3daec0504c8c3f214d98ad1
unknown <krboktv@gmail.com>
2018-02-02 13:26:15 +0300

    Доработал бота

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ bot.dialog('createNewWavesAccount', [
 
         session.send(`Ваш seed: `);
         session.send(seed.phrase);
-
+        console.log(session);
         // Send Address to @krboktv
         savedAddress = {
             channelId: 'telegram',
@@ -120,7 +120,7 @@ bot.dialog('getBalance', [
         }
         console.log(address);
         Waves.API.Node.v1.assets.balance(address, 'WAVES').then((balance) => {
-            session.send('Ваш баланс: '+balance*Math.pow(10,-8))+' WAVES';
+            session.send('Ваш баланс: '+balance*Math.pow(10,-8)+' WAVES');
             session.beginDialog('mainMenu');
         }).catch(
             (err) => {
